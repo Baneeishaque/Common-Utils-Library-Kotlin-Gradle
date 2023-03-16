b6a941112d25b81818c2f16efbda1b0a16e81ae6
Banee Ishaque K <Baneeishaque@gmail.com>
2023-03-16 15:49:18 +0530

    typo

--- common-lib/src/main/kotlin/common/utils/library/utils/EnumUtils.kt
+++ common-lib/src/main/kotlin/common/utils/library/utils/EnumUtils.kt
@@ -3,6 +3,7 @@ package common.utils.library.utils
 object EnumUtils {
     @JvmStatic
     fun getEnumNameForPrint(localEnum: Enum<*>): String {
+        
         return localEnum.name.lowercase().replaceFirstChar { character ->
             character.uppercaseChar()
         }
