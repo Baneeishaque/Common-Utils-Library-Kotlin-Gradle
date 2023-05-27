c23fc04c88dbc989075fccea45d7a6973f5a3098
Banee Ishaque K <baneeishaque@gmail.com>
2023-05-27 21:17:03 +0530

    error for IsOkModel

--- common-lib/src/main/kotlin/common/utils/library/models/IsOkModel.kt
+++ common-lib/src/main/kotlin/common/utils/library/models/IsOkModel.kt
@@ -1,3 +1,3 @@
 package common.utils.library.models
 
-data class IsOkModel<T>(val isOK: Boolean, val data: T? = null)
+data class IsOkModel<T>(val isOK: Boolean, val data: T? = null, val error: String? = null)
