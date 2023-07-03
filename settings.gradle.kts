d2a674c06f6264df45c6a04c0f44abab54217fa4
Banee Ishaque K <Baneeishaque@gmail.com>
2023-07-03 15:38:14 +0530

    Build Script Standardization

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,10 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        maven {
+            url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+        }
+    }
+}
 rootProject.name = "Common-Utils-Library"
 include(":common-lib")
